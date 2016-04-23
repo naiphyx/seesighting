6c723ca6ef04097135b2b0c7bb8d19f9cc8b6e0b
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2016-04-23 15:32:17 +0200

    add browsersync for html and scss

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browsersync = require('browser-sync').create();
 
 gulp.task('styles', function() {
 	return gulp.src('app/css/*.scss')
 			.pipe(sass())
-			.pipe(gulp.dest('dist'));
+			.pipe(gulp.dest('dist'))
+			.pipe(browsersync.stream());
 }); 
 
 gulp.task('copy', function() {
 	return gulp.src('app/index.html')
 	.pipe(gulp.dest('dist'));
-}),
+});
 
-gulp.task('default', ['styles', 'copy']);
+gulp.task('serve', function() {
+	browsersync.init({
+		server: './app'
+	});
+
+	gulp.watch('app/css/*.scss', ['styles']);
+	gulp.watch('app/*.html').on('change', browsersync.reload);
+});
+
+gulp.task('default', ['styles', 'copy', 'serve']);
