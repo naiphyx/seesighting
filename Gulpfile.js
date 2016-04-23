c6dfd2448e91a730e474eea5c58483db69242f5a
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2016-04-23 16:28:41 +0200

    basic page layout

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,22 +5,30 @@ var browsersync = require('browser-sync').create();
 gulp.task('styles', function() {
 	return gulp.src('app/css/*.scss')
 			.pipe(sass())
-			.pipe(gulp.dest('dist'))
+			.pipe(gulp.dest('dist/css'))
 			.pipe(browsersync.stream());
 }); 
 
+gulp.task('assets', function() {
+	return gulp.src('app/assets/**/*')
+			.pipe(gulp.dest('dist'))
+			.pipe(browsersync.stream());
+})
+
 gulp.task('copy', function() {
 	return gulp.src('app/index.html')
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest('dist'))
+	.pipe(browsersync.stream());
 });
 
 gulp.task('serve', function() {
 	browsersync.init({
-		server: './app'
+		server: './dist'
 	});
 
 	gulp.watch('app/css/*.scss', ['styles']);
-	gulp.watch('app/*.html').on('change', browsersync.reload);
+	gulp.watch('app/*.html', ['copy']);
+	gulp.watch('app/assets/**/*', ['assets']);
 });
 
-gulp.task('default', ['styles', 'copy', 'serve']);
+gulp.task('default', ['styles', 'copy', 'assets', 'serve']);
