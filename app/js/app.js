c68d06c1b4f19cf940428dde6444913195e326a5
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-04-27 12:39:51 +0200

    feat: build in map, add jquery

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
 var SparqlClient = require('sparql-client');
+const $ = require('jquery')
 
 var endpoint = 'http://dbpedia.org/sparql';
 var query = 'select distinct ?Concept from <http://dbpedia.org> where {[] a ?Concept} limit 100';
@@ -8,3 +9,16 @@ console.log("Query: " + query);
 client.query(query, function (error, results) {
     console.log(results);
 });
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
+$(document).ready(function(){
+
+})
