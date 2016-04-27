e4ea26244e4bdd78fe5d7940217077820bdfa779
Bernhard Steger <bernhard.steger@gmail.com>
2016-04-27 18:03:58 +0200

    refactoring, centers map on pageload

--- app/js/app.js
+++ app/js/app.js
@@ -1,54 +1,87 @@
 const SparqlClient = require('sparql-client')
 const $ = require('jquery')
 
-var endpoint = 'http://dbpedia.org/sparql';
-var query = " PREFIX dcterms:  <http://purl.org/dc/terms/>\
-              PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\
-              SELECT DISTINCT (str(?city) as ?City) (str(?label) as ?Attractions)\
-              WHERE { \
-                ?entity skos:broader <http://dbpedia.org/resource/Category:Tourism_by_city> .\
-                ?places skos:broader ?entity .\
-                ?places rdfs:label ?city .\
-              FILTER langMatches(lang(?label), 'en').\
-                ?attractions dcterms:subject ?places .\
-                ?attractions rdfs:label ?label .\
-              FILTER regex(str(?city), '(Tourist|Visitor)')\
-              }\
-              ORDER BY ASC(?City)\
-            ";
-var client = new SparqlClient(endpoint);
-console.log("Query to " + endpoint);
-console.log("Query: " + query);
 
-var x, y
+// <-------------- Variables ---------------->
+var endpoint = 'http://dbpedia.org/sparql'
+var client = new SparqlClient(endpoint)
+var results[]
+var lat, lng
 
-client.query(query, function (error, results) {
-  if (error) {
-    console.log(error)
-  } else {
-    console.log(results)
-  }
+
+// <----------------------------------------->
+$(document).ready(function(){
+
+  // sets the lat and lng of the current user
+  getLocation()
+
+  // centers the map at the current position of the user
+  centerMaptoGeolocation()
 })
 
 
-
-
-
-function centerMaptoGeolocation(position){
-	map.setCenter({lat: position.coords.latitude, lng: position.coords.longitude})
-  console.log("lat: " + position.coords.latitude + " long: " + position.coords.longitude)
-}
-
-
+// <------------ User Location --------------->
 function getLocation() {
   if (navigator.geolocation) {
-      return navigator.geolocation.getCurrentPosition(centerMaptoGeolocation);
+      return navigator.geolocation.getCurrentPosition(setCoords);
   } else {
     console.log("Geolocation is not supported by this browser.")
   }
 }
 
 
-$(document).ready(function(){
+function setCoords(position) {
+  lat = position.coords.latitude
+  long = position.coords.longitude
+}
 
-})
+
+// <---------------- Map --------------------->
+function centerMaptoGeolocation(){
+	map.setCenter({lat: lat, lng: long})
+}
+
+
+function setMarkers() {
+
+}
+
+
+// <------------ DB queries ------------------>
+// runs a query against the DB with given params
+function queryDB(query) {
+  client.query(query, function (error, results) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log(results)
+    }
+  })
+}
+
+
+function getSightsInProximity() {
+  var query = ""
+
+
+  queryDB(query)
+}
+
+
+function getSightsByCity(city) {
+  var query = " PREFIX dcterms:  <http://purl.org/dc/terms/>\
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\
+                SELECT DISTINCT (str(?city) as ?City) (str(?label) as ?Attractions)\
+                WHERE { \
+                  ?entity skos:broader <http://dbpedia.org/resource/Category:Tourism_by_city> .\
+                  ?places skos:broader ?entity .\
+                  ?places rdfs:label '?city' .\
+                FILTER langMatches(lang(?label), 'en').\
+                  ?attractions dcterms:subject ?places .\
+                  ?attractions rdfs:label ?label .\
+                FILTER regex(str(?city), '(Tourist|Visitor)')\
+                }\
+                ORDER BY ASC(?City)\
+                "
+  queryDB(query)
+}
