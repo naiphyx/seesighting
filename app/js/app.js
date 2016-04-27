0cc926a6ad9aa21c8f0af886571e3dfe19c8bd0b
magdawi <mwimmer.mmt-b2014@fh-salzburg.ac.at>
2016-04-27 16:56:16 +0200

    added query

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,20 @@ var SparqlClient = require('sparql-client');
 const $ = require('jquery')
 
 var endpoint = 'http://dbpedia.org/sparql';
-var query = 'select distinct ?Concept from <http://dbpedia.org> where {[] a ?Concept} limit 100';
+var query = " PREFIX dcterms:  <http://purl.org/dc/terms/>\
+              PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\
+              SELECT DISTINCT (str(?city) as ?City) (str(?label) as ?Attractions)\
+              WHERE { \
+                ?entity skos:broader <http://dbpedia.org/resource/Category:Tourism_by_city> .\
+                ?places skos:broader ?entity .\
+                ?places rdfs:label ?city .\
+              FILTER langMatches(lang(?label), 'en').\
+                ?attractions dcterms:subject ?places .\
+                ?attractions rdfs:label ?label .\
+              FILTER regex(str(?city), '(Tourist|Visitor)')\
+              }\
+              ORDER BY ASC(?City)\
+            ";
 var client = new SparqlClient(endpoint);
 console.log("Query to " + endpoint);
 console.log("Query: " + query);
