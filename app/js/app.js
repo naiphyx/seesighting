95836a28c034ed0611ff8747faba07b37731e353
Bernhard Steger <bernhard.steger@gmail.com>
2016-04-27 11:00:33 +0200

    removed gulp-browserify

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,10 @@
 var SparqlClient = require('sparql-client');
-// var util = require('util');
-var endpoint = 'http://dbpedia.org/sparql';
 
-// Get the leaderName(s) of the given citys
-// if you do not bind any city, it returns 10 random leaderNames
-var query = "SELECT * FROM <http://dbpedia.org> WHERE { ?city <http://dbpedia.org/property/leaderName> ?leaderName } LIMIT 10";
+var endpoint = 'http://dbpedia.org/sparql';
+var query = 'select distinct ?Concept from <http://dbpedia.org> where {[] a ?Concept} limit 100';
 var client = new SparqlClient(endpoint);
 console.log("Query to " + endpoint);
 console.log("Query: " + query);
-client.query(query)
-  //.bind('city', 'db:Chicago')
-  //.bind('city', 'db:Tokyo')
-  // .bind('city', '<http://dbpedia.org/resource/Vienna>')
-  .execute(function(error, results) {
-  console.log(results);
-  // process.stdout.write(util.inspect(arguments, null, 20, true)+"\n");1
+client.query(query, function (error, results) {
+    console.log(results);
 });
