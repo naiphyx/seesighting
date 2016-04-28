0619f592350bc3540795365be14a19c2bdcadb39
Bernhard Steger <bernhard.steger@gmail.com>
2016-04-28 20:17:08 +0200

    seclude javascript in different files

--- app/js/app.js
+++ app/js/app.js
@@ -8,7 +8,7 @@ var results = []
 var lat, long
 
 
-// <----------------------------------------->
+// <------------- onload ----------------------->
 $(document).ready(function(){
 
   // sets the lat and lng of the current user
@@ -41,67 +41,3 @@ $(document).ready(function(){
       }
     }
   }
-
-
-// <------------ User Location --------------->
-function getLocation() {
-  if (navigator.geolocation) {
-      return navigator.geolocation.getCurrentPosition(setCoords);
-  } else {
-    console.log("Geolocation is not supported by this browser.")
-  }
-}
-
-
-function setCoords(position) {
-  lat = position.coords.latitude
-  long = position.coords.longitude
-
-  // centers the map at the current position of the user
-  map.setCenter({lat: lat, lng: long})
-}
-
-
-// <---------------- Map --------------------->
-function setMarkers() {
-
-}
-
-
-// <------------ DB queries ------------------>
-// runs a query against the DB with given params
-function queryDB(query) {
-  client.query(query, function (error, results) {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log(results)
-      showResults(results)
-    }
-  })
-}
-
-
-function getSightsInProximity() {
-  var query = ""
-
-
-  queryDB(query)
-}
-
-
-function getSightsByCity(city) {
-  city = city.replace(" ", "_")
-  var query = "PREFIX dcterms:  <http://purl.org/dc/terms/>\
-                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\
-                SELECT DISTINCT (str(?label) as ?Label) (str(?lat) as ?Lat) (str(?long) as ?Long)\
-                WHERE { \
-                  ?sight dct:subject <http://dbpedia.org/resource/Category:Visitor_attractions_in_" + city + "> .\
-                FILTER langMatches(lang(?label), 'en').\
-                  ?sight rdfs:label ?label .\
-                  ?sight geo:lat ?lat .\
-                  ?sight geo:long ?long\
-                }\
-                ORDER BY ASC(?Label)"
-  queryDB(query)
-}
